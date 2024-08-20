c6b56f260baba30789d0fe2e01fc79c7a5efb616
HB <156240575+hbuhrer@users.noreply.github.com>
2024-08-20 10:16:03 -0600

    Added initial landing page code. This is just a placeholder to start development from. Project initialized, Nuxt3 + TailwindCSS to start with. Structure configured.

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div class="flex flex-col min-h-screen bg-gray-200">
+      <NuxtPage />
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <section
+      class="flex flex-col max-w-[800px] text-center mt-6 px-8 py-8 mx-auto justify-center items-center font-mono"
+    >
+      <img
+        src="/public/images/cyntech-group-logo.svg"
+        alt="Logo"
+        class="mx-auto my-12"
+      />
+      <h1
+        class="mx-auto text-pretty bg-gradient-to-b text-[#036] bg-clip-text text-center text-3xl leading-tight text-transparent lg:max-w-3xl lg:text-4xl"
+      >
+        Landing Page Development Placeholder
+      </h1>
+      <p
+        class="mx-auto mt-4 max-w-xl text-center text-lg antialiased text-[#f6c71e] bg-[#036] p-4 rounded-sm"
+      >
+        This is a placeholder for the landing page development.
+      </p>
+    </section>
+  </div>
+</template>
