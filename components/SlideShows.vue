0b46d258ba2c95ed6a0afd14cb2329e8611287c1
HB <156240575+hbuhrer@users.noreply.github.com>
2024-08-24 02:19:22 -0600

    Started initial fix on slide transitions.

--- components/SlideShows.vue
+++ components/SlideShows.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen relative flex flex-col items-center justify-center bg-[#1b3664] overflow-hidden">
     <div 
-      :class="['absolute inset-0 w-full h-full transition-transform duration-300 ease-in-out bg-center bg-no-repeat bg-cover', backgroundClass]" 
+      :class="['absolute inset-0 w-full h-full transition-transform duration-500 ease-in-out bg-center bg-no-repeat bg-cover', backgroundClass]" 
       :style="backgroundStyle"
     ></div>
     <div class="relative z-10 flex flex-col w-full items-center justify-center">
@@ -69,8 +69,8 @@ const startSlideShow = (business) => {
         backgroundStyle.value = { backgroundImage: `url(${imageGalleries[business][currentImageIndex.value]})` };
         backgroundClass.value = 'translate-x-0';
       }, 500);
-    }, 2000);
-  }, 300);
+    }, 2500);
+  }, 500);
 };
 
 const stopSlideShow = () => {
@@ -81,7 +81,7 @@ const stopSlideShow = () => {
   setTimeout(() => {
     backgroundStyle.value = { backgroundColor: '#1b3664' };
     backgroundClass.value = 'translate-x-0';
-  }, 300);
+  }, 500);
 };
 
 defineExpose({ startSlideShow, stopSlideShow });
